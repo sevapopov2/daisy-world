837e50ace48c17a7f313ee0fd7aa114fca5dcf59
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-18 17:46:36 +0300

    Use views names instead of URLs, like in Django.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 """Routes, same as url confs in Django."""
 
 
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, url_for
 
 from app import app
 from app.forms import LoginForm
@@ -22,5 +22,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Вход', form=form)
