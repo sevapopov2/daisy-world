c0e67db902da3b8d86e4fe17b3a86265e892106f
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-18 17:16:23 +0300

    Login form. Fix 405 error. Add flashed messages with login information.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 """Routes, same as url confs in Django."""
 
 
-from flask import render_template
+from flask import flash, redirect, render_template
 
 from app import app
 from app.forms import LoginForm
@@ -15,8 +15,12 @@ def index():
     return render_template('index.html', title='Главная Страница', user=user)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Login view."""
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Вход', form=form)
