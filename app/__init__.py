03c9abb197fcc66c6408af3b2b64c56c81f62b50
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-24 13:02:30 +0300

    Database. Add database and migration settings.

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@
 
 from config import Config
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import models, routes
--- config.py
+++ config.py
@@ -2,8 +2,13 @@
 
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     """Class that stores secret key and other configuration."""
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'daisy_world.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
