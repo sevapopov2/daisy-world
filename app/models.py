dc3fe7b13f8b97d93f69522d84c81c79d254bafa
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-25 16:28:53 +0300

    Database. Add basic books table model.

--- app/models.py
+++ app/models.py
@@ -10,7 +10,17 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    books = db.relationship('Book', backref='published_by', lazy='dynamic')
 
     def __repr__(self):
         """Format representation of data in class when printed. Used in debug."""
         return '<User {}>'.format(self.username)
+
+
+class Book(db.Model):
+    """Book model."""
+
+    book_id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100))
+    book_author = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.userid'))
