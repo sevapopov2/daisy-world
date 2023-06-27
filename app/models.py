73d44cc6306c84b4a8f64b8c44a9b708cc7bc7c6
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-27 20:14:20 +0300

    Models. Remove book table. Change user id's column name in users table.

--- app/models.py
+++ app/models.py
@@ -9,11 +9,11 @@ from app import db, login
 class User(db.Model, UserMixin):
     """User db model."""
 
-    userid = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    books = db.relationship('Book', backref='published_by', lazy='dynamic')
+
 
     def set_password(self, password):
         """Set password hash method."""
@@ -27,19 +27,6 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         """Format representation of data in class when printed. Used in debug."""
         return '<User {}>'.format(self.username)
-
-
-class Book(db.Model):
-    """Book model."""
-
-    book_id = db.Column(db.Integer, primary_key=True)
-    book_name = db.Column(db.String(100))
-    book_author = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.userid'))
-
-    def __repr__(self):
-        """Class representation when printing."""
-        return '<Book {}>'.format(self.book_name)
 
 
 @login.user_loader
