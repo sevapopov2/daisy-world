c720116cb7f8e32fd9c4b3e54f9b3d9051c3525b
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-27 20:21:57 +0300

    Refactor migrations. Remove book table.

--- daisy-world.py
+++ daisy-world.py
@@ -1,10 +1,10 @@
 """File to make the app working by importing it."""
 
 from app import app, db
-from app.models import Book, User
+from app.models import User
 
 
 @app.shell_context_processor
 def make_shell_context():
     """Open flask shell with imported db."""
-    return {'db': db, 'Book': Book, 'User': User}
+    return {'db': db, 'User': User}
