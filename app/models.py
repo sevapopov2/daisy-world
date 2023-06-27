c6b40d0bf935f46bf7459ba5b312f583a481ac18
Vsevolod Popov <sevapopov13@gmail.com>
2023-06-27 12:02:06 +0300

    Models. Add password hashing.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 """Database models structure in ORM."""
 
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from app import db
 
 
@@ -11,6 +13,15 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     books = db.relationship('Book', backref='published_by', lazy='dynamic')
+
+    def set_password(self, password):
+        """Set password hash method."""
+        set_password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Check password hash method."""
+        return check_password_hash(self.password_hash, password)
+
 
     def __repr__(self):
         """Format representation of data in class when printed. Used in debug."""
